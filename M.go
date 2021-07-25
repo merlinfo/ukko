2004f64543fc47029c28c7ea4e3cce00769ae03d
geremachek <jonahgrongstad@gmail.com>
2021-07-25 07:58:29 -0700

    comments

--- M.go
+++ M.go
@@ -18,18 +18,26 @@ func main() {
 		args = os.Args[1:]
 	)
 
+	// get path env var, or use default
+
 	if sb := os.Getenv("SPELLBOOK"); sb != "" {
 		spellbook = sb
 	}
 
 	notation = spellbook + " ;spellbook "
 
+	// string of summons to open all of the file in args
+
 	for i := len(args)-1; i >= 0; i-- {
 		notation += args[i] + " ;summon "
 	}
 
+	// run it!
+
 	merlin := exec.Command("merlin", notation)
 	
+	// with std...
+
 	merlin.Stdout = os.Stdout
 	merlin.Stdin = os.Stdin
 	merlin.Stderr = os.Stderr
